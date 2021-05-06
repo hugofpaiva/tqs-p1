318d98e864a07cba9f21a56453c0c9cf5a6d7b09
hugofpaiva <hugofpaiva@ua.pt>
2021-05-06 02:43:31 +0100

    Preparation of Models and Services

---
+++ web-application/src/app/communication/models/request.ts
@@ -0,0 +1,8 @@
+export class Request {
+    id: number;
+    date: Date;
+    cacheResponse: string;
+    airQualityLocationAccessed: string;
+    latitude: number;
+    longitude: number;
+  }
--- web-application/src/environments/environment.prod.ts
+++ web-application/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+import { HttpHeaders } from "@angular/common/http";
+
 export const environment = {
+  httpOptions: {
+    headers: new HttpHeaders({'Content-Type': 'application/json'})
+  },
+  baseURL: 'http://spring:8080/api/',
   production: true
 };
