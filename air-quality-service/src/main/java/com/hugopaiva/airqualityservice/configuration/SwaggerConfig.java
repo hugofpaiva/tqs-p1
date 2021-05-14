91771d7e52721c80e52ab8a406e6f15d63d4f130
hugofpaiva <hugofpaiva@ua.pt>
2021-05-14 15:41:05 +0100

    Minor fixes with Swagger

--- air-quality-service/src/main/java/com/hugopaiva/airqualityservice/configuration/SwaggerConfig.java
+++ air-quality-service/src/main/java/com/hugopaiva/airqualityservice/configuration/SwaggerConfig.java
@@ -2,23 +2,27 @@ package com.hugopaiva.airqualityservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 
 @Configuration
+@Import(SpringDataRestConfiguration.class)
 public class SwaggerConfig {
 
     @Bean
     public Docket airQualityServiceApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .paths(regex("/api.*"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
     }
